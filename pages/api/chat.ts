172eff343a81dc9f0b5b553eaf3e1d8921605942
libaice <libaice147@gmail.com>
2023-07-29 16:41:19 +0800

    update componment

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,6 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { makeChain } from '@/utils/makechain';
+import { pinecone } from '@/utils/pinecone-client';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,4 +22,33 @@ export default async function handler(
     return res.status(400).json({ message: "Question is required" });
   }
   const sanitizedQuestion = question.trim().replaceAll("\n", " ");
+
+  try {
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+    /* create vectorstore*/
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings({}),
+      {
+        pineconeIndex: index,
+        textKey: 'text',
+        namespace: PINECONE_NAME_SPACE, //namespace comes from your config folder
+      },
+    );
+
+    //create chain
+    const chain = makeChain(vectorStore);
+    //Ask a question using chat history
+    const response = await chain.call({
+      question: sanitizedQuestion,
+      chat_history: history || [],
+    });
+
+    console.log('response', response);
+    res.status(200).json(response);
+  } catch (error: any) {
+    console.log('error', error);
+    res.status(500).json({ error: error.message || 'Something went wrong' });
+  }
+
 }
